5d741bb974da3f694bc92fea04a6fb3bf7e5484a
figuenu <nuno.figueiredo@portugen.com>
2014-04-24 14:33:28 +0100

    Cache matrix R script to be submitted
    Cache matrix R script to be submitted:
    - makeCacheMatrix function
    - cacheSolve function
    - 1 test example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a cacheable matrix to be used by cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Uses solve to invert a cacheable matrix if not present in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
+## Example for testing
+c<-rbind(c(1, -1/4), c(-1/4, 1))
+d<-makeCacheMatrix(c)
+d
+e<-cacheSolve(d)
+e
+cacheSolve(makeCacheMatrix(c))
+c %*% e
+
+
+
+
